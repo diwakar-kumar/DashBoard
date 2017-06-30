40ffb2e1be23e5979669673f32dba943cb62a0a6
2016ht13065 <2016ht13065@wilp.bits-pilani.ac.in>
2017-06-30 22:39:02 +0530

    Fix Build Error in app code

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+
 var index = require('./app/expressAppServer/routes/index');
 var catalog = require('./app/expressAppServer/routes/catalog');
 var port = 3333;
@@ -8,4 +9,6 @@ app.listen(port, 'localhost', function(){
 })
 
 app.use('/', index);
+app.use('/catalog', catalog); 
 console.log('Server started at Port 3333');
+module.exports = app;
