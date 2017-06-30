570c84b1e2ea3fe875ede9c82f36673e1dc29eaf
Dheeraj karki <dheerajkarki1790@outlook.com>
2017-06-30 16:11:56 +0530

    Create a Coding standard for application

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-"use strict"
+
 var http = require('http');
-
+var port = 3333;
 http.createServer(function(request, response){
-    response.writeHead(200, {'Content-Type':'text/plain'});
+    var statusCode = 200; 
+    response.writeHead(statusCode, {'Content-Type':'text/plain'});
     response.end('Hello World!');
-
-}).listen(3333);
-
+}).listen(port);
 console.log('Server started at Port 3333');
